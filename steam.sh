dc268d02bd4db9d05b025026e3f0b91dbadc127d
es <160103673+es-j3@users.noreply.github.com>
2025-01-10 03:11:15 +0000

    update patched proton to 9.0.3

--- steam.sh
+++ steam.sh
@@ -9,13 +9,13 @@ install_dependencies() {
 # Patches Proton for UE Games https://gitlab.winehq.org/wine/wine/-/merge_requests/5213/diffs#d5bcbaed4ae76fff7d2641017921e07798a7da0e_807_807
 patch_proton() {
     cd /tmp
-    fetch https://github.com/coolerguy71/Steam-BSD-Runtime/releases/download/Release/wine-proton-9.0.2-amd64.pkg
-    fetch https://github.com/coolerguy71/Steam-BSD-Runtime/releases/download/Release/wine-proton-9.0.2-i386.pkg
+    fetch https://github.com/es-j3/Steam-BSD-Runtime/releases/download/proton-patch-v1.0.1/wine-proton-9.0.3-amd64.pkg
+    fetch https://github.com/es-j3/Steam-BSD-Runtime/releases/download/proton-patch-v1.0.1/wine-proton-9.0.3-i386.pkg
     su -l root -c 'pkg remove -y wine-proton'
-    su -l root -c 'pkg install -y /tmp/wine-proton-9.0.2-amd64.pkg' 
+    su -l root -c 'pkg install -y /tmp/wine-proton-9.0.3-amd64.pkg' 
     cd
     /usr/local/share/wine/pkg32.sh remove -y wine-proton
-    /usr/local/share/wine/pkg32.sh install -y /tmp/wine-proton-9.0.2-i386.pkg
+    /usr/local/share/wine/pkg32.sh install -y /tmp/wine-proton-9.0.3-i386.pkg
 }
 
 if zenity --question --text="Would you like to install wine-devel, winetricks and wine-proton? They are dependencies needed."; then
