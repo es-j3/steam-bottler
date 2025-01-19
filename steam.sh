04f7026cf8127582eb7b959ee45245decfa7bde4
es <160103673+es-j3@users.noreply.github.com>
2025-01-19 21:45:05 +0000

    Update steam.sh

--- steam.sh
+++ steam.sh
@@ -9,13 +9,13 @@ install_dependencies() {
 # Patches Proton for UE Games https://gitlab.winehq.org/wine/wine/-/merge_requests/5213/diffs#d5bcbaed4ae76fff7d2641017921e07798a7da0e_807_807
 patch_proton() {
     cd /tmp
-    fetch https://github.com/es-j3/Steam-BSD-Runtime/releases/download/proton-patch-v1.0.2/wine-proton-9.0.4-amd64.pkg
-    fetch https://github.com/es-j3/Steam-BSD-Runtime/releases/download/proton-patch-v1.0.2/wine-proton-9.0.4-i386.pkg
+    fetch https://github.com/es-j3/Steam-BSD-Runtime/releases/download/proton-patch-v1.0.3/proton-bleeding-edge-amd64.pkg
+    fetch https://github.com/es-j3/Steam-BSD-Runtime/releases/download/proton-patch-v1.0.3/proton-bleeding-edge-i386.pkg
     su -l root -c 'pkg remove -y wine-proton'
-    su -l root -c 'pkg install -y /tmp/wine-proton-9.0.4-amd64.pkg' 
+    su -l root -c 'pkg install -y /tmp/proton-bleeding-edge-amd64.pkg' 
     cd
     /usr/local/share/wine/pkg32.sh remove -y wine-proton
-    /usr/local/share/wine/pkg32.sh install -y /tmp/wine-proton-9.0.4-i386.pkg
+    /usr/local/share/wine/pkg32.sh install -y /tmp/proton-bleeding-edge-i386.pkg
 }
 
 # Launches Steam with OSS instead of Pulse (for source games)
@@ -44,7 +44,7 @@ else
     exit 0
 fi
 
-if zenity --question --text="Would you like to patch Proton that fixes various Unreal Engine and Unity games? (For FreeBSD 14.2)"; then
+if zenity --question --text="Would you like to install a bleeding-edge version of Proton? (With UE Multithread Patch, FreeBSD 14.2) "; then
     patch_proton
 else
     zenity --info --text="Alright, let's move on."
